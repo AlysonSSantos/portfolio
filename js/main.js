aaa3f3a668f317122ca032a0a6b064fc2cd1274d
AlysonSSantos <alysonsantos080@gmail.com>
2021-12-08 11:38:11 -0300

    estrutura dos arquivos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const abrirSidebar = () => {
+    document.getElementById("menuLateral").style.width="15rem";
+}
+
+const fecharSidebar = () => {
+    document.getElementById("menuLateral").style.width=0;
+}
