b935cd420e8103d153cdc648559fab68f272e1ba
AlysonSSantos <alysonsantos080@gmail.com>
2022-01-19 12:04:55 -0300

    portifolio do desenvolvedor

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ const fecharSidebar = () => {
     document.getElementById("menuLateral").style.width=0;
 }
 
+const redirecionarAoHome = () => {
+    window.scrollTo(0, 0)
+}
+
+const redirecionarSobre = () => {
+    window.scrollTo(0, 679)
+}
 
 
+const redirecionarAConhecimentos = () => {
+    window.scrollTo(0, 857)
+}
 
+const redirecionarAosProjetos = () => {
+    window.scrollTo(0, 1180)
+}
+
+const redirecionarAoContate_me = () => {
+    window.scrollTo(0, 1550)
+}
