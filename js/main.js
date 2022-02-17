5bd041958cd0e0e2ad6cbefd12f28952ae90a1e8
AlysonSSantos <alysonsantos080@gmail.com>
2022-02-17 11:29:20 -0300

    Portifolio - Alyson S

--- js/main.js
+++ js/main.js
@@ -8,22 +8,42 @@ const fecharSidebar = () => {
 
 const redirecionarAoHome = () => {
     window.scrollTo(0, 0)
+    document.getElementById3("bottomNavTextHome").style.color="#4D5ACB"
+    const setColor = setTimeout(function(){
+        document.getElementById("bottomNavTextSobre").style.color="#fff"
+    }, 2000)
 }
 
 const redirecionarAoSobre = () => {
     location.href="#sobre"
+    document.getElementById("bottomNavTextSobre").style.color="#4D5ACB"
+    const setColor = setTimeout(function(){
+        document.getElementById("bottomNavTextSobre").style.color="#fff"
+    }, 2000)
 }
 
 const redirecionarAConhecimentos = () => {
     location.href="#conhecimentos"
+    document.getElementById("bottomNavTextConhecimentos").style.color="#4D5ACB"
+    const setColor = setTimeout(function(){
+        document.getElementById("bottomNavTextConhecimentos").style.color="#fff"
+    }, 2000)
 }
 
 const redirecionarAosProjetos = () => {
     location.href="#projetos"
+    document.getElementById("bottomNavTextProjetos").style.color="#4D5ACB"
+    const setColor = setTimeout(function(){
+        document.getElementById("bottomNavTextProjetos").style.color="#fff"
+    }, 2000)
 }
 
 const redirecionarAoContate_me = () => {
     location.href="#contatar"
+    document.getElementById("bottomNavTextContate").style.color="#4D5ACB"
+    const setColor = setTimeout(function(){
+        document.getElementById("bottomNavTextContate").style.color="#fff"
+    }, 2000)
 }
 
 
